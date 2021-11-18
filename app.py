46b69c0ef0a90b906e4dcca4489ff02ea87f3e0d
ortonrocks <81026566+ortonrocks@users.noreply.github.com>
2021-11-18 14:38:57 +0800

    bot五個功能

--- app.py
+++ app.py
@@ -1,129 +1,64 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  2 21:16:35 2021
+import os.path
+import tempfile
 
-@author: Ivan
-版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
-
-Line Bot聊天機器人
-第三章 互動回傳功能
-推播push_message與回覆reply_message
-"""
-#載入LineBot所需要的套件
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-import re
-from linebot.models import (
-    MessageEvent,VideoMessage,TextMessage, StickerMessage, StickerSendMessage, ConfirmTemplate, TemplateSendMessage, MessageAction, URIAction, LocationMessage
- )
+from flask import Flask
 app = Flask(__name__)
 
-# 必須放上自己的Channel Access Token
+from flask import request, abort
+from linebot import  LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, PostbackEvent, TextSendMessage, ImagemapSendMessage, BaseSize, MessageImagemapAction, URIImagemapAction, ImagemapArea, TemplateSendMessage, ButtonsTemplate, DatetimePickerTemplateAction
+from urllib.parse import parse_qsl
+import datetime
+from linebot.models import (
+    MessageEvent,QuickReplyButton,QuickReply,VideoMessage,TextMessage, StickerMessage, StickerSendMessage, ConfirmTemplate, TemplateSendMessage, MessageAction, URIAction, LocationMessage
+ )
+
 line_bot_api = LineBotApi('6LtqwU4k493Gys589ikza9GzxWgrHjJFIcDGc21+JcMAALUjLd2xLzGRJft575QbIOeaUEedDr6QMf4mormSu0bCA8QuUTGj0kC0Im1qNsovhsMLv8tHwJjE2PkLvA44E8ckPuLRtWlTu3sNq+rNmwdB04t89/1O/w1cDnyilFU=')
-# 必須放上自己的Channel Secret
 handler = WebhookHandler('62fedcb34c8415668774fd2ccdb5d73c')
 
-line_bot_api.push_message('U738f70cb25916b736d9abe88f11d01f2', TextSendMessage(text='你可以開始了'))
-
-# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
+
+
+
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-#訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
+
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = text=event.message.text
-    if re.match('看評論',message):
-         flex_message = TextSendMessage(text='以下有雷，請小心',
-                                quick_reply=QuickReply(items=[
-                                    QuickReplyButton(action=MessageAction(label="PTT", text="https://www.ptt.cc/bbs/movie/index.html")),
-                                    QuickReplyButton(action=MessageAction(label="DCARD", text="https://www.dcard.tw/f/movie")),
-                                    
-                                ]))
-         line_bot_api.reply_message(event.reply_token, flex_message)
+    mtext = event.message.text
+    if mtext == '@圖片地圖':
+        sendImgmap(event)
 
+    elif mtext == '@日期時間':
+        sendDatetime(event)
 
-    elif message == 'test':
+    elif mtext == '@儲存人臉特徵':
         user_camera_open(event)
-    elif re.match('你誰',message):
+
+    elif mtext == '@看電影評論':
+        comment_show(event)
+
+    elif mtext =='@彩蛋':
         line_bot_api.reply_message(event.reply_token,TextSendMessage('我誰~~~！'))
-    elif re.match('你是誰',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('我誰~~~！當然是你的電影大幫手'))
-        
-    elif re.match('Hi',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('安安'))
-    elif re.match('hi',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('尼好 肥宅'))
-        
-    elif re.match('哈囉',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('哈囉肥宅'))
-    elif re.match('你好',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('哈囉肥宅'))
-    elif re.match('你好阿',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('哈囉肥宅'))
 
-    elif re.match('摸',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('東踏取蜜!'))
 
-    elif re.match('幹',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('沒禮貌! '))
-    elif re.match('三小',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('沒禮貌! '))
-    elif re.match('87',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('沒禮貌! '))
-    elif re.match('我愛你',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('我也愛你 '))
-    elif re.match('愛你',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('我也愛你 '))
-    elif re.match('你好帥',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('你現在才知道哦 '))
-    elif re.match('我好帥',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('你說付錢的時候嗎?真的滿帥的 '))
-        
-    elif re.match('我帥嗎',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('滿帥的，只不過差我一點')) 
-    elif re.match('好餓',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('我也很餓'))
-    elif re.match('餓',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('我也很餓'))
-    elif re.match('早安',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('早安！今天真是美好的一天'))
-    elif re.match('早',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('早安！今天真是美好的一天'))
-    elif re.match('想睡了',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('晚安，祝你有個好夢~!'))
-    elif re.match('晚安',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('晚安，祝你有個好夢~!'))
 
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage('哈哈，你沒猜中關鍵字'))
-#123
+
+
 
 @handler.add(MessageEvent,message=(VideoMessage))
 def handle_content_message(event):
-    static_tmp_path=''
+    static_tmp_path='./resources'
     if isinstance(event.message,VideoMessage):
         ext='mp4'
     else:
@@ -151,9 +86,136 @@ def handle_content_message(event):
 
 
 
+@handler.add(PostbackEvent)  #PostbackTemplateAction觸發此事件
 
-#主程式
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+def comment_show(event):
+    flex_message = TextSendMessage(text='以下有雷，請小心',
+                                   quick_reply=QuickReply(items=[
+                                       QuickReplyButton(action=MessageAction(label="PTT",
+                                                                             text="https://www.ptt.cc/bbs/movie/index.html")),
+                                       QuickReplyButton(action=MessageAction(label="DCARD",
+                                                                             text="https://www.dcard.tw/f/movie")),
+
+                                   ]))
+    line_bot_api.reply_message(event.reply_token, flex_message)
+
+
+
+def handle_postback(event):
+    backdata = dict(parse_qsl(event.postback.data))  #取得data資料
+    if backdata.get('action') == 'sell':
+        sendData_sell(event, backdata)
+
+
+def user_camera_open(event):
+    queries = ConfirmTemplate(
+        text="人臉註冊請錄製5秒以上的影片上傳，請問是否開啟相機?",
+        actions=[
+            URIAction(
+                label='開啟相機',
+                uri='line://nv/camera'
+            ),
+            MessageAction(label='不需要', text='不需要')
+
+        ])
+
+    temp_msg = TemplateSendMessage(alt_text='確認訊息', template=queries)
+
+    line_bot_api.reply_message(event.reply_token, temp_msg)
+
+
+
+
+
+def sendImgmap(event):  #圖片地圖
+    try:
+        image_url = 'https://i.imgur.com/Yz2yzve.jpg'  #圖片位址
+        imgwidth = 1040  #原始圖片寛度一定要1040
+        imgheight = 300
+        message = ImagemapSendMessage(
+            base_url=image_url,
+            alt_text="圖片地圖範例",
+            base_size=BaseSize(height=imgheight, width=imgwidth),  #圖片寬及高
+            actions=[
+                MessageImagemapAction(  #顯示文字訊息
+                    text='你點選了紅色區塊！',
+                    area=ImagemapArea(  #設定圖片範圍:左方1/4區域
+                        x=0,
+                        y=0,
+                        width=imgwidth*0.25,
+                        height=imgheight
+                    )
+                ),
+                URIImagemapAction(  #開啟網頁
+                    link_uri='http://www.e-happy.com.tw',
+                    area=ImagemapArea(  #右方1/4區域(藍色1)
+                        x=imgwidth*0.75,
+                        y=0,
+                        width=imgwidth*0.25,
+                        height=imgheight
+                    )
+                ),
+            ]
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendDatetime(event):  #日期時間
+    try:
+        message = TemplateSendMessage(
+            alt_text='日期時間範例',
+            template=ButtonsTemplate(
+                thumbnail_image_url='https://i.imgur.com/VxVB46z.jpg',
+                title='日期時間示範',
+                text='請選擇：',
+                actions=[
+                    DatetimePickerTemplateAction(
+                        label="選取日期",
+                        data="action=sell&mode=date",  #觸發postback事件
+                        mode="date",  #選取日期
+                        initial="2020-10-01",  #顯示初始日期
+                        min="2020-10-01",  #最小日期
+                        max="2021-12-31"  #最大日期
+                    ),
+                    DatetimePickerTemplateAction(
+                        label="選取時間",
+                        data="action=sell&mode=time",
+                        mode="time",  #選取時間
+                        initial="10:00",
+                        min="00:00",
+                        max="23:59"
+                    ),
+                    DatetimePickerTemplateAction(
+                        label="選取日期時間",
+                        data="action=sell&mode=datetime",
+                        mode="datetime",  #選取日期時間
+                        initial="2020-10-01T10:00",
+                        min="2020-10-01T00:00",
+                        max="2021-12-31T23:59"
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendData_sell(event, backdata):  #Postback,顯示日期時間
+    try:
+        if backdata.get('mode') == 'date':
+            dt = '日期為：' + event.postback.params.get('date')  #讀取日期
+        elif backdata.get('mode') == 'time':
+            dt = '時間為：' + event.postback.params.get('time')  #讀取時間
+        elif backdata.get('mode') == 'datetime':
+            dt = datetime.datetime.strptime(event.postback.params.get('datetime'), '%Y-%m-%dT%H:%M')  #讀取日期時間
+            dt = dt.strftime('{d}%Y-%m-%d, {t}%H:%M').format(d='日期為：', t='時間為：')  #轉為字串
+        message = TextSendMessage(
+            text=dt
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+if __name__ == '__main__':
+    app.run()
---
+++ readme.txt
@@ -0,0 +1,12 @@
+linebot可互動功能：
+'@圖片地圖':sendImgmap(event)
+
+'@日期時間': sendDatetime(event)
+
+'@儲存人臉特徵':user_camera_open(event)
+
+'@看電影評論':comment_show(event)
+
+'@彩蛋'
+
+更新日期：11/18
